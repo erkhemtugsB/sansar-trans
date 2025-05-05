07b8bcdb6bee0256c4df4fbf467447f8d967891c
Erkhemtugs <erhemevlee@gmail.com>
2025-05-05 17:33:53 -0500

    added contact to every page

---
+++ contact.js
@@ -0,0 +1,11 @@
+    // Function to show the popup and overlay
+    function showPopup() {
+        document.getElementById('popup').style.display = 'block';
+        document.getElementById('overlay').style.display = 'block';
+      }
+  
+      // Function to hide the popup and overlay
+      function hidePopup() {
+        document.getElementById('popup').style.display = 'none';
+        document.getElementById('overlay').style.display = 'none';
+      }
